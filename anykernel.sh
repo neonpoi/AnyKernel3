f6c1fc983e3b9a20e54b0d84138d597b0b8cd12a
neonpoi <zyanagamingg@gmail.com>
2020-12-10 05:39:25 -0800

    HMP: Update & Add Banner

--- anykernel.sh
+++ anykernel.sh
@@ -4,61 +4,102 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=
 do.devicecheck=1
 do.modules=0
-do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
-supported.patchlevels=
+device.name1=X00T
+device.name2=X00TD
+device.name3=ASUS_X00T
+device.name4=ASUS_X00TD
+device.name5=ASUS_X00TDA
+supported.versions=8.1 - 11.0
+supported.patchlevels=2018-04 -
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/soc/c0c4000.sdhci/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+# Mount partitions as rw
+mount /system;
+mount /vendor;
+mount -o remount,rw /system;
+mount -o remount,rw /vendor;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+chmod -R 750 $ramdisk/*;
+chmod -R 755 $ramdisk/sbin;
+chown -R root:root $ramdisk/*;
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
+#Remove old kernel stuffs from ramdisk
+ui_print " "
+ rm -rf $ramdisk/init.special_power.sh
+ rm -rf $ramdisk/init.darkonah.rc
+ rm -rf $ramdisk/init.spectrum.rc
+ rm -rf $ramdisk/init.spectrum.sh
+ rm -rf $ramdisk/init.boost.rc
+ rm -rf $ramdisk/init.trb.rc
+ rm -rf $ramdisk/init.azure.rc
+ rm -rf $ramdisk/init.PBH.rc
+ rm -rf $ramdisk/init.Pbh.rc
+ rm -rf $ramdisk/init.overdose.rc
+
 backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+remove_line init.rc "import /init.darkonah.rc";
+remove_line init.rc "import /init.spectrum.rc";
+remove_line init.rc "import /init.boost.rc";
+remove_line init.rc "import /init.trb.rc"
+remove_line init.rc "import /init.azure.rc"
+remove_line init.rc "import /init.PbH.rc"
+remove_line init.rc "import /init.Pbh.rc"
+remove_line init.rc "import /init.overdose.rc"
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+ui_print " ";
+# rearm perfboostsconfig.xml
+if [ ! -f /vendor/etc/perf/perfboostsconfig.xml ]; then
+	mv /vendor/etc/perf/perfboostsconfig.xml.bak /vendor/etc/perf/perfboostsconfig.xml;
+	mv /vendor/etc/perf/perfboostsconfig.xml.bkp /vendor/etc/perf/perfboostsconfig.xml;
+fi
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+# rearm commonresourceconfigs.xml
+if [ ! -f /vendor/etc/perf/commonresourceconfigs.xml ]; then
+	mv /vendor/etc/perf/commonresourceconfigs.xml.bak /vendor/etc/perf/commonresourceconfigs.xml;
+	mv /vendor/etc/perf/commonresourceconfigs.xml.bkp /vendor/etc/perf/commonresourceconfigs.xml;
+fi
+
+# rearm targetconfig.xml
+if [ ! -f /vendor/etc/perf/targetconfig.xml ]; then
+	mv /vendor/etc/perf/targetconfig.xml.bak /vendor/etc/perf/targetconfig.xml;
+	mv /vendor/etc/perf/targetconfig.xml.bkp /vendor/etc/perf/targetconfig.xml;
+fi
+
+# rearm targetresourceconfigs.xml
+if [ ! -f /vendor/etc/perf/targetresourceconfigs.xml ]; then
+	mv /vendor/etc/perf/targetresourceconfigs.xml.bak /vendor/etc/perf/targetresourceconfigs.xml;
+	mv /vendor/etc/perf/targetresourceconfigs.xml.bkp /vendor/etc/perf/targetresourceconfigs.xml;
+fi
+
+# rearm powerhint.xml
+if [ ! -f /vendor/etc/powerhint.xml ]; then
+	mv /vendor/etc/powerhint.xml.bak /vendor/etc/powerhint.xml;
+	mv /vendor/etc/powerhint.xml.bkp /vendor/etc/powerhint.xml;
+fi
 
 # end ramdisk changes
 
 write_boot;
-## end install
 
+## end install
